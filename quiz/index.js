984fb2f23b5f8e82ad327772259a24791da18747
gaogao <gaoyh@cimu.com.cn>
2016-01-27 16:13:32 +0800

    修改keydown事件为keyup

--- quiz/index.js
+++ quiz/index.js
@@ -85,7 +85,7 @@ $(function(){
         var compiled_template = _.template(temp)(quiz);
         $("#app").append(compiled_template);
         var answers = []
-        $(".fill input").on("keydown",function(e){
+        $(".fill input").on("keyup",function(e){
           var index = $(this).data("index");
           var question_id = $(this).data("question")
           var answers = quiz.your_answers
